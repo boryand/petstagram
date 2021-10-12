6ec71693a3b87900fc330dbeaeda77aaab668b95
boryand <boryand>
2021-10-12 16:19:10 +0200

    Set up the contentful client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import client from "./client/client";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    client.getEntries().then(function (entries) {
+      // log the title for all the entries that have it
+      console.log(entries);
+    });
+  }, []);
+
+  return <div>petstagram</div>;
 }
 
 export default App;
